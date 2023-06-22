120a5808f26fbf21aa070988a84356e6daca1c4c
Alessandro <defranceschi_a@yahoo.com>
2023-06-22 18:44:55 +0200

    modified signer in snippets

--- token-with-apr/snippets.sh
+++ token-with-apr/snippets.sh
@@ -1,4 +1,4 @@
-USER_PEM="~/wallets/development.pem"
+SIGNER="--pem=~/wallets/development.pem"
 
 PROXY="https://devnet-gateway.multiversx.com"
 CHAIN_ID="D"
@@ -10,7 +10,7 @@ REWARD_TOKEN="XCUMB-da0e35"
 
 deploy() {
     mxpy contract deploy --bytecode="output/staking.wasm" \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=100000000 \
     --send --wait-result \
     --proxy=${PROXY} --chain=${CHAIN_ID} || return
@@ -22,7 +22,7 @@ build() {
 
 upgrade() {
     mxpy contract upgrade ${CONTRACT_ADDRESS} --project=${PROJECT} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=120000000 \
     --send --wait-result \
     --proxy=${PROXY} --chain=${CHAIN_ID} || return
@@ -30,7 +30,7 @@ upgrade() {
 
 call() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "unstake_multiple" \
     --arguments 6 \
@@ -53,7 +53,7 @@ getgenerico() {
 
 set_admin() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "set_admin" \
     --arguments erd1e9nnlhee5rvn9k8y3ysmtsx490h8nw9jels5vjdlezl72yrtg3yq6vvj3r \
@@ -62,7 +62,7 @@ set_admin() {
 }
 set_reward_token() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "set_reward_token" \
     --arguments str:${REWARD_TOKEN} \
@@ -71,7 +71,7 @@ set_reward_token() {
 }
 set_staking_token() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "set_staking_token" \
     --arguments str:${STAKING_TOKEN} \
@@ -80,7 +80,7 @@ set_staking_token() {
 }
 set_apr() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "set_apr" \
     --arguments 50 \
@@ -89,7 +89,7 @@ set_apr() {
 }
 deposit_rewards() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "ESDTTransfer" \
     --arguments str:${REWARD_TOKEN} 10000000000000000000000 str:deposit_rewards \
@@ -98,7 +98,7 @@ deposit_rewards() {
 }
 stake() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "ESDTTransfer" \
     --arguments str:${STAKE_TOKEN} 10000000000000000000000 str:stake \
@@ -107,7 +107,7 @@ stake() {
 }
 unstake() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=20000000 \
     --function "unstake" \
     --arguments 1000000000000000000 \
@@ -116,7 +116,7 @@ unstake() {
 }
 claim_rewards() {
     mxpy contract call ${CONTRACT_ADDRESS} \
-    --recall-nonce --pem=${USER_PEM} \
+    --recall-nonce ${SIGNER} \
     --gas-limit=10000000 \
     --function "claim_rewards" \
     --send --wait-result \
